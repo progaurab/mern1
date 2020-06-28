7a6c9f8f0dae1e4f4d4c25f35c70d6464cab709c
Instill Learning <progaurab@gmail.com>
2020-06-28 16:51:43 +0530

    initial commit

---
+++ nodeserver/app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// use Routes 
+app.use('/api/books', books);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://blog.logrocket.com/mern-stack-a-to-z-part-1/
